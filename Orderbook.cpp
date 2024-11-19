8e638a26cd862802213242fd670ed394cd99d0ce
Ian Andersen Ng <ianandersen@Ians-Laptop.local>
2024-11-19 16:53:58 +0800

    Add match trades

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -40,31 +40,85 @@ class OrderBook {
         } 
     }
 
-    // TODO : Implement this function
+    bool validateMatchTrade(Price bidPrice, Price askPrice){
+
+        if(bids_.empty() || asks_.empty()){
+            return false;    
+        }
+
+        if(bidPrice < askPrice){
+            return false;
+        }
+
+        return true;
+    }
+
     Trades matchTrades(){
         Trades trades;
         trades.reserve(orders_.size());
         
         while (true) {
-
-            if(bids_.empty() || asks_.empty()){
-                break;
-            }
-
             auto& [bidPrice, bids] = *bids_.begin();
             auto& [askPrice, asks] = *asks_.begin();
 
-            if(bidPrice < askPrice){
+            if (!validateMatchTrade(bidPrice, askPrice)) {
                 break;
             }
 
             while (bids.size() && asks.size()) {
                 auto& bid = bids.front();
                 auto& ask = asks.front();
-            }
 
+                Quantity quantity = std::min(bid->getRemainingQuantity(), ask->getRemainingQuantity());
+
+                bid -> fill(quantity);
+                ask -> fill(quantity);
+
+                if (bid -> isFilled()) {
+                    bids.pop_front();
+                    orders_.erase(bid -> getOrderId());
+                }
+
+                if (ask -> isFilled()) {
+                    asks.pop_front();
+                    orders_.erase(ask -> getOrderId());
+                }
+                
+                Trade matchedTrade = Trade(
+                    TradeInfo{bid -> getOrderId(), bid -> getPrice(), quantity},
+                    TradeInfo{ask -> getOrderId(), ask -> getPrice(), quantity}
+                );
+
+                trades.push_back(matchedTrade);
+            }
         }
-        
+
+
+
+        cancelFillAndKillOrders();
+        return trades;
+    }
+
+    void cancelFillAndKillOrders(){
+        if(!bids_.empty()) {
+            auto& [_, bids] = *bids_.begin();
+            auto& order = bids.front();
+            if(order->getOrderType() == OrderType::FillAndKill){
+                cancelOrder(order->getOrderId());
+            }
+        }
+
+        if(!asks_.empty()) {
+            auto& [_, asks] = *asks_.begin();
+            auto& order = asks.front();
+            if(order->getOrderType() == OrderType::FillAndKill){
+                cancelOrder(order->getOrderId());
+            }
+        }
+    } 
+    
+    void cancelOrder(OrderId orderId){
+        return;
     }
 };
 
--- classes/Order.h
+++ classes/Order.h
@@ -45,6 +45,8 @@ class Order {
 
         remainingQuantity_ -= quantity;
     }
+
+    bool isFilled() const { return getRemainingQuantity() == 0; }
     
 };
 
