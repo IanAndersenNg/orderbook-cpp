c9f44b5a4e8ea8aaf2465e495e0e07721fc65d3d
Ian Andersen Ng <ianandersen@Ians-Laptop.local>
2024-11-19 17:01:03 +0800

    init Add Order

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -14,112 +14,122 @@ class OrderBook {
             OrderPointers::iterator location_;
         };
 
-    std::map<Price, OrderPointers, std::greater<Price>> bids_;
-    std::map<Price, OrderPointers, std::less<Price>> asks_;
-    std::unordered_map<OrderId, OrderEntry> orders_;
+        std::map<Price, OrderPointers, std::greater<Price>> bids_;
+        std::map<Price, OrderPointers, std::less<Price>> asks_;
+        std::unordered_map<OrderId, OrderEntry> orders_;
 
-    bool canMatch(Side side, Price price){
-        if(side == Side::Buy){
-            if(asks_.empty()){
+        bool canMatch(Side side, Price price){
+            if(side == Side::Buy){
+                if(asks_.empty()){
+                    return false;
+                }
+
+                // dereference iterator to get the first element
+                // std::pair<const Price, OrderPointers>
+
+                const auto& [bestAsk, _] = *asks_.begin(); 
+                return price >= bestAsk;
+
+            } else {
+                if(bids_.empty()){
+                    return false;
+                }
+
+                const auto& [bestBid, _] = *bids_.begin();
+                return price <= bestBid;
+            } 
+        }
+
+        bool validateMatchTrade(Price bidPrice, Price askPrice){
+
+            if(bids_.empty() || asks_.empty()){
+                return false;    
+            }
+
+            if(bidPrice < askPrice){
                 return false;
             }
 
-            // dereference iterator to get the first element
-            // std::pair<const Price, OrderPointers>
+            return true;
+        }
 
-            const auto& [bestAsk, _] = *asks_.begin(); 
-            return price >= bestAsk;
+        Trades matchTrades(){
+            Trades trades;
+            trades.reserve(orders_.size());
+            
+            while (true) {
+                auto& [bidPrice, bids] = *bids_.begin();
+                auto& [askPrice, asks] = *asks_.begin();
 
-        } else {
-            if(bids_.empty()){
-                return false;
+                if (!validateMatchTrade(bidPrice, askPrice)) {
+                    break;
+                }
+
+                while (bids.size() && asks.size()) {
+                    auto& bid = bids.front();
+                    auto& ask = asks.front();
+
+                    Quantity quantity = std::min(bid->getRemainingQuantity(), ask->getRemainingQuantity());
+
+                    bid -> fill(quantity);
+                    ask -> fill(quantity);
+
+                    if (bid -> isFilled()) {
+                        bids.pop_front();
+                        orders_.erase(bid -> getOrderId());
+                    }
+
+                    if (ask -> isFilled()) {
+                        asks.pop_front();
+                        orders_.erase(ask -> getOrderId());
+                    }
+                    
+                    Trade matchedTrade = Trade(
+                        TradeInfo{bid -> getOrderId(), bid -> getPrice(), quantity},
+                        TradeInfo{ask -> getOrderId(), ask -> getPrice(), quantity}
+                    );
+
+                    trades.push_back(matchedTrade);
+                }
+            }
+            cancelFillAndKillOrders();
+            return trades;
+        }
+
+        void cancelFillAndKillOrders(){
+            if(!bids_.empty()) {
+                auto& [_, bids] = *bids_.begin();
+                auto& order = bids.front();
+                if(order->getOrderType() == OrderType::FillAndKill){
+                    cancelOrder(order->getOrderId());
+                }
             }
 
-            const auto& [bestBid, _] = *bids_.begin();
-            return price <= bestBid;
+            if(!asks_.empty()) {
+                auto& [_, asks] = *asks_.begin();
+                auto& order = asks.front();
+                if(order->getOrderType() == OrderType::FillAndKill){
+                    cancelOrder(order->getOrderId());
+                }
+            }
         } 
-    }
 
-    bool validateMatchTrade(Price bidPrice, Price askPrice){
-
-        if(bids_.empty() || asks_.empty()){
-            return false;    
+        void cancelOrder(OrderId orderId){
+            return;
         }
 
-        if(bidPrice < askPrice){
-            return false;
-        }
+    public :
 
-        return true;
-    }
-
-    Trades matchTrades(){
-        Trades trades;
-        trades.reserve(orders_.size());
-        
-        while (true) {
-            auto& [bidPrice, bids] = *bids_.begin();
-            auto& [askPrice, asks] = *asks_.begin();
-
-            if (!validateMatchTrade(bidPrice, askPrice)) {
-                break;
+        Trades AddOrder(OrderPointer order){
+            if(orders_.contains(order->getOrderId())){
+                return {};
             }
 
-            while (bids.size() && asks.size()) {
-                auto& bid = bids.front();
-                auto& ask = asks.front();
-
-                Quantity quantity = std::min(bid->getRemainingQuantity(), ask->getRemainingQuantity());
-
-                bid -> fill(quantity);
-                ask -> fill(quantity);
-
-                if (bid -> isFilled()) {
-                    bids.pop_front();
-                    orders_.erase(bid -> getOrderId());
-                }
-
-                if (ask -> isFilled()) {
-                    asks.pop_front();
-                    orders_.erase(ask -> getOrderId());
-                }
-                
-                Trade matchedTrade = Trade(
-                    TradeInfo{bid -> getOrderId(), bid -> getPrice(), quantity},
-                    TradeInfo{ask -> getOrderId(), ask -> getPrice(), quantity}
-                );
-
-                trades.push_back(matchedTrade);
+            if(order -> getOrderType() == OrderType::FillAndKill && 
+                !canMatch(order->getSide(), order->getPrice())){
+                return {};
             }
         }
-
-
-
-        cancelFillAndKillOrders();
-        return trades;
-    }
-
-    void cancelFillAndKillOrders(){
-        if(!bids_.empty()) {
-            auto& [_, bids] = *bids_.begin();
-            auto& order = bids.front();
-            if(order->getOrderType() == OrderType::FillAndKill){
-                cancelOrder(order->getOrderId());
-            }
-        }
-
-        if(!asks_.empty()) {
-            auto& [_, asks] = *asks_.begin();
-            auto& order = asks.front();
-            if(order->getOrderType() == OrderType::FillAndKill){
-                cancelOrder(order->getOrderId());
-            }
-        }
-    } 
-    
-    void cancelOrder(OrderId orderId){
-        return;
-    }
 };
 
 int main() {
